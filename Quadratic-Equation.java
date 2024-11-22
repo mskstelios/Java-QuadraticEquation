aac1a13e3f0589a754c1627c65bd4f6c15f73cad
Stelios Miskedakis <153899847+mskstelios@users.noreply.github.com>
2024-11-22 17:16:50 +0200

    Create Quadratic-Equation.java

---
+++ Quadratic-Equation.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+class Quadratic_Equation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a: ");
+        int a = scanner.nextInt();
+        System.out.println("Enter b: ");
+        int b = scanner.nextInt();
+        System.out.println("Enter c: ");
+        int c = scanner.nextInt();
+
+        int D = (int) ((Math.pow(b, 2)) - 4 * a * c);
+
+        String format = a + "x^2 + " + b + "x + " + c;
+        System.out.printf("Format: %dx^2 + %dx + %d%n%n", a, b, c);
+        System.out.println("D: " + D);
+
+        if (D > 0) {
+            System.out.println("D > 0:");
+            double x1 = (-b + Math.sqrt(D)) / (2.0 * a);
+            double x2 = (-b - Math.sqrt(D)) / (2.0 * a);
+            System.out.printf("x1: " + x1 + "\n");
+            System.out.println("x2: " + x2);
+        } else if (D == 0) {
+            System.out.println("D = 0:");
+            double x1 = -b / (2.0 * a);
+            System.out.printf("x1: " + x1);
+        } else {
+            System.out.println("No roots. Because D < 0");
+        }
+
+    }
+}
